b3d8eda2ddbad711370f12808210e80295cb39f8
Nitish Kumar Das <nitishdas992@gmail.com>
2022-09-30 15:19:28 +0530

    initial commit

---
+++ lib/sign_in.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:login_dribble/sign_in_back.dart';
+import 'package:login_dribble/sign_up.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<SignInPage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<SignInPage> {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: CustomPaint(
+        size: Size(width, (width * 2).toDouble()),
+        //Background Curve
+        painter: SignInBackground(),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0, left: 35.0),
+                child: Row(
+                  children: const [Icon(Icons.arrow_back_ios)],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 80.0, left: 40),
+                child: Row(
+                  children: const [
+                    Text(
+                      "Welcome\nBack",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 32,
+                          color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+
+              //Input Fields
+              Padding(
+                padding: const EdgeInsets.only(left: 35.0, top: 120, right: 50),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Email',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Password',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    //Sign in button
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          "Sign In",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 22),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            shape: const CircleBorder(),
+                            padding: const EdgeInsets.all(20),
+                            primary: const Color.fromARGB(255, 67, 72, 76),
+                            onPrimary: Colors.black,
+                          ),
+                          child: const Icon(Icons.arrow_forward_sharp,
+                              color: Colors.white),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 110,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const SignUp()));
+                          },
+                          child: const Text(
+                            "Sign Up",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ),
+                        const Text(
+                          "Forgot Password?",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.underline,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
